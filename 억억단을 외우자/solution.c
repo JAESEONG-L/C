d85b7eb0ae94a8443483e6b40b87d4dcb1947a10
JAESEONG-L <97182403+JAESEONG-L@users.noreply.github.com>
2023-05-08 19:05:26 +0900

    Update solution.c
    Pre-calculated power of primes now covers 2, 3, 5, 7, and 11.

--- 억억단을 외우자/solution.c
+++ 억억단을 외우자/solution.c
@@ -10,7 +10,7 @@
 int* solution(int e, int starts[], size_t starts_len) {
     static int answers[E_MAX];
     static int dp[E_MAX]= {1, 2, 2, };  // One appears once, two appears twice, and three appears twice in the matrix.
-    int powerNums[8][23]= {{0, }, {0, }, {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304}, {1, 3, 9, 27, 81, 243, 729, 2187, 6561, 19683, 59049, 177147, 531441, 1594323, 4782969, }, {0, }, {1, 5, 25, 125, 625, 3125, 15625, 78125, 390625, 1953125, }, {0, }, {1, 7, 49, 343, 2401, 16807, 117649, 823543, }};
+    int powerNums[12][23]= {{0, }, {0, }, {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304}, {1, 3, 9, 27, 81, 243, 729, 2187, 6561, 19683, 59049, 177147, 531441, 1594323, 4782969, }, {0, }, {1, 5, 25, 125, 625, 3125, 15625, 78125, 390625, 1953125, }, {0, }, {1, 7, 49, 343, 2401, 16807, 117649, 823543, }, {0, }, {0, }, {0, }, {1, 11, 121, 1331, 14641, 161051, 1771561, }};
     int primes[PRIME_MAX+1];
     int primeCount= 0;
     
@@ -30,7 +30,7 @@ int* solution(int e, int starts[], size_t starts_len) {
     
     primes[primeCount++]= 1;
     
-    int pnCounts[8]= {0, 0, 22, 14, 0, 9, 0, 7};    // Look up for powerNums[][].
+    int pnCounts[12]= {0, 0, 22, 14, 0, 9, 0, 7, 0, 0, 0, 6};   // Look up for powerNums[][].
     
     // Calculates dp[].
     for(int num= 4; num<=e; num++) {
@@ -49,7 +49,7 @@ int* solution(int e, int starts[], size_t starts_len) {
         
         int primeFactor= primes[i];
         
-        if(primeFactor<=7) {
+        if(primeFactor<=11) {
             int left= 1, right= pnCounts[primeFactor];
             int exponent;
             
